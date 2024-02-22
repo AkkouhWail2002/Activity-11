842b920ce83a3678da1183e313b406424606169b
AkkouhWail2002 <160769964+AkkouhWail2002@users.noreply.github.com>
2024-02-22 16:14:42 +0100

    Add files via upload

---
+++ Activite11/index.js
@@ -0,0 +1,6 @@
+document.getElementById("img").onclick=function(){
+    document.getElementById("menu").style.transform="translateX(0%)";
+}
+document.getElementById("close").onclick=function(){
+    document.getElementById("menu").style.transform="translateX(-100%)";
+}
